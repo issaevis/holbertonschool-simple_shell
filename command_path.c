848642af73eaa6102480215a170ca49ad8d15d7d
Patr1k00 <shalsipatrik00@gmail.com>
2023-04-26 03:52:58 -0700

    This is command_path file of Simple Shell Project

---
+++ command_path.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+char *command_path(char *cmd)
+{
+    int i = 0;
+    char *path = strdup(_getenv("PATH"));
+    char *new_path = NULL, *token;
+    struct stat buffer;
+    char *array_path[100];
+
+    new_path = malloc(sizeof(char) * 100);
+
+    if (_getenv("PATH")[0] == ':')
+    {
+        if (stat(cmd, &buffer) == 0)
+        {
+            return (strdup(cmd));
+        }
+    }
+
+    /*Copy the words into the array*/
+    token = strtok(path, ":");
+    while (token != NULL)
+    {
+        array_path[i] = token;
+        i++;
+        token = strtok(NULL, ":");
+    }
+    array_path[i] = NULL;/*Ends the array_path*/
+
+    for (i = 0; array_path[i]; i++)
+	{
+		strcpy(new_path, array_path[i]);
+		strcat(new_path, "/");
+		strcat(new_path, cmd);
+		strcat(new_path, "\0");
+		if (stat(new_path, &buffer) == 0)
+		{
+			free(path);
+			return (new_path);
+		}
+		else
+			new_path[0] = 0;
+	}
+
+	free(path);
+	free(new_path);
+	if (stat(cmd, &buffer) == 0)
+		return (strdup(cmd));
+
+	return (NULL);
+}
