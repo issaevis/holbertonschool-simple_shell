cb0ede351704ced6960f8d6a0c24caafcee6a907
issaevis <evis.topollaj@gmail.com>
2023-04-27 03:58:59 -0700

    Added the file again

--- command_path.c
+++ command_path.c
@@ -1,56 +1,39 @@
 #include "main.h"
 /**
- * command_path - function
- * @cmd: char
- * Return: NULL
+ * command_path - Function that finds the path from environ
+ *
+ * @cmd: the command
+ *
+ * Return: Returns a full path
  */
 char *command_path(char *cmd)
 {
-<<<<<<< HEAD
-    int i = 0;
-    char *path = strdup(_getenv("PATH"));
-    char *new_path = NULL, *token = strtok(path, ":");
-    struct stat buffer;
-    char *array_path[100];
-
-    new_path = malloc(sizeof(char) * 100);
-    if (path == NULL)
-	    if (stat(cmd, &buffer) == 0)
-		    return (_strdup(cmd));
-    while (token != NULL)
-    {
-        array_path[i] = token;
-        i++;
-        token = strtok(NULL, ":");
-    }
-    array_path[i] = NULL;/*Ends the array_path*/
-    for (i = 0; array_path[i]; i++)
-=======
+	char *path = _strdup(_getenv("PATH"));
+	char *token = strtok(path, ":");
+	char *path_array[100];
+	char *new_path = NULL;
+	struct stat buf;
 	int i = 0;
-	char *path = strdup(_getenv("PATH")), *new_path = NULL, *token, *a_path[100];
-	struct stat buffer;
 
 	new_path = malloc(sizeof(char) * 100);
-	if (_getenv("PATH")[0] == ':')
-	{
-	if (stat(cmd, &buffer) == 0)
-		return (strdup(cmd));/*Copy the words into the array*/
-	token = strtok(path, ":");
+	if (path == NULL)
+		if (stat(cmd, &buf) == 0)
+			return (_strdup(cmd));
 	while (token != NULL)
 	{
-	a_path[i] = token;
-	i++;
-	token = strtok(NULL, ":");
+		path_array[i] = token;
+		i++;
+		token = strtok(NULL, ":");
 	}
-	a_path[i] = NULL;/*Ends the array_path*/
-	for (i = 0; a_path[i]; i++;
->>>>>>> e5f87fcfab9b0d7cb1c42f27f6ceb4d7713141c8
+	path_array[i] = NULL;
+
+	for (i = 0; path_array[i]; i++)
 	{
-		strcpy(new_path, a_path[i]);
-		strcat(new_path, "/");
-		strcat(new_path, cmd);
-		strcat(new_path, "\0");
-		if (stat(new_path, &buffer) == 0)
+		_strcpy(new_path, path_array[i]);
+		_strcat(new_path, "/");
+		_strcat(new_path, cmd);
+		_strcat(new_path, "\0");
+		if (stat(new_path, &buf) == 0)
 		{
 			free(path);
 			return (new_path);
@@ -60,7 +43,7 @@ char *command_path(char *cmd)
 	}
 	free(path);
 	free(new_path);
-	if (stat(cmd, &buffer) == 0)
-		return (strdup(cmd));
+	if (stat(cmd, &buf) == 0)
+		return (_strdup(cmd));
 	return (NULL);
 }
